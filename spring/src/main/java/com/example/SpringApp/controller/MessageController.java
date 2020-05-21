7a5f6414817ad78c3c45604a66f870d7717f07ff
O'Lik <55744134+OlyaPancyreva@users.noreply.github.com>
2020-05-21 12:31:03 +0300

    Create MessageController.java

---
+++ spring/src/main/java/com/example/SpringApp/controller/MessageController.java
@@ -0,0 +1,58 @@
+package com.example.SpringApp.controller;
+
+import com.example.SpringApp.domain.Message;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("message")
+public class MessageController {
+    private final MessageRepo messageRepo;
+
+
+    @Autowired
+    public MessageController(MessageRepo messageRepo) {
+        this.messageRepo = messageRepo;
+    }
+
+
+    @GetMapping
+    public List<Message> list() {
+        return messageRepo.findAll();
+    }
+
+    @GetMapping("{id}")
+    public Message getOne(@PathVariable("id") Message message) {
+        return message;
+    }
+
+    @PostMapping
+    public Message create(@RequestBody Message message) {
+        return messageRepo.save(message);
+    }
+
+    @PutMapping("{id}")
+    public Message update(@PathVariable("id") Message messageFromDb,
+                          @RequestBody Message message)
+    {
+        BeanUtils.copyProperties(message, messageFromDb, "id");
+        return messageRepo.save(messageFromDb);
+    }
+
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable("id") Message message) {
+        messageRepo.delete(message);
+
+    }
+
+    @MessageMapping("/changeMessage")
+    @SendTo("/topic/activity")
+    public Message change(Message message) {
+        return messageRepo.save(message);
+    }
+}
